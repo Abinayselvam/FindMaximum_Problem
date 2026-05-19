eca42c83d3d54683f2865537396d787bace6d364
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-19 17:21:40 +0530

    Added the Refactor 1 features

--- Day13/src/Main.java
+++ Day13/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Test  the maximum.");
+
+        MaxTest<Integer> intMax = new MaxTest<>(10, 20, 30);
+        intMax.testMaximum();
+
+        MaxTest<Float> floatMax = new MaxTest<>(2.3f, 5.6f, 1.1f);
+        floatMax.testMaximum();
+
+        MaxTest<String> stringMax = new MaxTest<>("Apple", "Peach", "Banana");
+        stringMax.testMaximum();
+
     }
 }
--- Day13/src/MaxTest.java
+++ Day13/src/MaxTest.java
@@ -1,2 +1,34 @@
-public class MaxTest {
+public class MaxTest<T extends Comparable<T>> {
+
+        T a, b, c;
+
+        public MaxTest(T a, T b, T c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public T testMaximum() {
+            return MaxTest.testMaximum(a, b, c);
+        }
+
+        public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+
+            T max = a;
+
+            if (b.compareTo(max) > 0)
+                max = b;
+
+            if (c.compareTo(max) > 0)
+                max = c;
+
+            printMax(max);
+
+            return max;
+        }
+
+        public static <T> void printMax(T max) {
+            System.out.println("Maximum value is: " + max);
+        }
+
 }
