2cb39d02913dba4200cb13207c23081f2bfcf2f5
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-19 17:06:46 +0530

    Added the feature UC2

--- Day13/src/Main.java
+++ Day13/src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Test  the maximum.");
+
+    public static float testMaximum(float a, float b, float c) {
+        float max = a;
+
+        if (b > max)
+            max = b;
+
+        if (c > max)
+            max = c;
+
+        return max;
     }
+
+        public static void main(String[] args) {
+            System.out.println(testMaximum(3.5f, 2.1f, 1.9f));
+            System.out.println(testMaximum(1.2f, 5.6f, 3.3f));
+            System.out.println(testMaximum(1.2f, 3.3f, 7.8f));
+        }
+
 }
